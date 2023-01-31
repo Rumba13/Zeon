941adc48c8cf3b90e1697585ca172ac5c0f99bbe
Kaban228 <cveremeev@yandex.by>
2023-01-31 21:06:22 +0300

    todo rename to sprite and fix all css

--- src/components/sprite/sprite.tsx
+++ src/components/sprite/sprite.tsx
@@ -8,6 +8,6 @@ export default function Sprite({ yOffset }: PropsType) {
         return <div style={{
             backgroundPositionY: yOffset
         }}
-          className="icon"></div> //TODO:rename classname to "sprite" and fix all css
+          className="sprite"></div> //TODO:rename classname to "sprite" and fix all css
 
 }
